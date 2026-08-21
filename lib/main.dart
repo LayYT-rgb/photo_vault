25e71319050939a20583b5b81b3a90cbc15995b6
LayYT-rgb <artequickly@gmail.com>
2026-08-21 15:25:05 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'screens/gallery_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(child: Text('Hello, it works!', style: TextStyle(fontSize: 24))),
-    ),
-  ));
+  runApp(const PhotoVaultApp());
+}
+
+class PhotoVaultApp extends StatelessWidget {
+  const PhotoVaultApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Photo Vault',
+      theme: ThemeData(colorSchemeSeed: Colors.indigo, useMaterial3: true),
+      home: const GalleryScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
